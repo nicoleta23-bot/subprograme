753547b323cfda857229aa6c4d440f3271464166
nicoleta <71027700+nicoleta23-bot@users.noreply.github.com>
2022-09-14 11:57:41 +0300

    Add files via upload

---
+++ ex3.py
@@ -0,0 +1,23 @@
+a=int(input('a='))
+b=int(input('b='))
+c=int(input('c='))
+d=int(input('d='))
+def subp(x):
+    s=0
+    for i in range(x):
+
+        s=(a/b)+(c/d)
+    if b==0 or d==0:    
+         print('multime vida')
+    return s
+print("suma este:",subp(a))
+
+def subp(x):
+    s=0
+    for i in range(x):
+
+        s=(a/b)*(c/d)
+    if b==0 or d==0:    
+         print('multime vida')
+    return s
+print("produsul este:",subp(a))
